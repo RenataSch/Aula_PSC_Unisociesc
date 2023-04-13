5a3bec510ac1118c0f04d22299c170d34ebadb9b
Renata <127457027+RenataSch@users.noreply.github.com>
2023-04-12 22:18:02 -0300

    Add files via upload

---
+++ Adicao.java
@@ -0,0 +1,12 @@
+package projetoPáscoa;
+
+public class Adicao extends Operacao{
+
+	public float calcula() {
+		return Numero1 + Numero2;	
+	}
+	public float calcula(int prm1, int prm2) {
+		return prm1 + prm2;
+	}
+
+}
---
+++ Multiplicacao.java
@@ -0,0 +1,11 @@
+package projetoPáscoa;
+
+public class Multiplicacao extends Operacao{
+
+	public float calcula () {
+		return Numero1 * Numero2;
+	}
+	public float calcula(int prm1, int prm2) {
+		return prm1 * prm2;
+	}
+}
